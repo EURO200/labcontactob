bbfe2f27dc51ffcf87af9d1a96727bf0b02cbaf3
Enrique Urcullo <enriquencido@gmail.com>
2019-08-23 20:37:02 -0400

    commit dos

---
+++ contacto/contacto/Models/contacto.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace contacto.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public enum Typecontact
+    {
+        telephone,
+        email,
+        facebook
+    }
+    public class contacto
+    {
+        [Key]
+        public int ContactID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public Typecontact Type { get; set; }
+        [Required]
+        [Display(Description ="Contact")]
+        public string ContactValue { get; set; }
+    }
+}
